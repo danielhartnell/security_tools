ded53ef004224069c141644616944fdd70bba91b
Daniel Hartnell <danielhartnell@gmail.com>
2017-11-30 21:06:01 -0800

    begin migration to classes for database interaction

---
+++ autobounty/database/company.py
@@ -0,0 +1,20 @@
+class Company:
+    def __init__(self, id=None, name=None, fqdn=None):
+        self.id = id,
+        self.name = name,
+        self.fqdn = fqdn
+
+    def find(self):
+        # Find one by company ID
+        pass
+
+    def find_all(self):
+        # Find all companies
+        pass
+
+    def update(self):
+        # Update x on n companies by y query
+        pass
+
+    def save(self):
+        pass
---
+++ autobounty/database/domain.py
@@ -0,0 +1,18 @@
+class Domain:
+    def __init__(self, fqdn=None):
+        self.fqdn = fqdn
+
+    def find(self):
+        # Find one by fqdn
+        pass
+
+    def find_all(self):
+        # Find all domains
+        pass
+
+    def update(self):
+        # Update x on n domains by y query
+        pass
+
+    def save(self):
+        pass
