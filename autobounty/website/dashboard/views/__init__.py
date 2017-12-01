66b3ba652ac9fe9589eab6c16d378566c38d30ca
Daniel Hartnell <danielhartnell@gmail.com>
2017-11-30 18:40:14 -0800

    refactor dashboard#index and other minor changes

--- autobounty/website/dashboard/views/__init__.py
+++ autobounty/website/dashboard/views/__init__.py
@@ -1,21 +1,15 @@
-from flask import render_template
+import datetime
 
+from flask import render_template
 from autobounty import database
 from autobounty.scanner import tasks
 from autobounty.website.dashboard import web
-import datetime
 
-
-# Todo: Use API backend when relevant
-# Find helper functions for repeating patterns
-# Sanitize user input
-# Could be fun to define a data type that will parse all inputs
 
 @web.route('/')
 @web.route('/dashboard')
 def index():
     companies = []
-    unique_companies = []
     search = database.find_all_companies()
     for result in search:
         if result['last_scan'] is not int:
@@ -24,40 +18,33 @@ def index():
         companies.append({
             'company_name': result['company_name'],
             'company_id': result['company_id'],
-            'last_scan': date,
-            'matches': database.count(result['company_name'])
+            'last_scan': date
         })
     unique_companies = list({v['company_name']: v for v in companies}.values())
-    # Stop adding match total to every company object (slow)
+    for c in unique_companies:
+        c['matches'] = database.count(c['company_name'])
+
+    # Look at an ordered dictionary
+    # cache_property decorator
     return render_template('companies.html',
                            companies=unique_companies,
                            title='Autobounty Dashboard')
 
-@web.route('/scan/headers')
-def scan_headers():
-    tasks.header_scan_subdomains.delay()
-    return 'Initiated Celery background task to scan all domains.'
 
-@web.route('/scan')
-def scan():
-    tasks.enumerate_subdomains.delay()
-    return 'Scanning subdomains'
-
-# Create a new route to view a specific org
-# Query DB for all domains under that org
-# Only show orgs with links on the dashboard
-@web.route('/<id>')
-@web.route('/dashboard/<id>')
-def dashboard(id):
-    domains = database.find_domains_by_company_id(id)
+@web.route('/<company_id>')
+@web.route('/dashboard/<company_id>')
+def dashboard(company_id):
+    domains = database.find_domains_by_company_id(company_id)
+    company_name = domains[0]['company_name']
     return render_template('domains.html',
                            domains=domains,
-                           title='Dashboard for a specific org')
+                           title='{} dashboard'.format(company_name))
 
-@web.route('/scan/<id>')
-@web.route('/dashboard/scan/<id>')
-def scan_results_web(id):
-    domain = database.find_domains_by_id(id)
+
+@web.route('/scan/<domain_id>')
+@web.route('/dashboard/scan/<domain_id>')
+def scan_results_web(domain_id):
+    domain = database.find_domains_by_id(domain_id)
     fqdn = domain['fqdn']
     scan_active = 'null'
     if domain['active'] is bool:
@@ -65,13 +52,19 @@ def scan_results_web(id):
             scan_active = 'True'
         else:
             scan_active = 'False'
-    #if domain['']
     return render_template('scan.html',
                            domain=domain,
                            active=scan_active,
                            title='Scan: {}'.format(fqdn))
 
-# Domain overview page
-    # Show domain response
-        # Response code or connection status (timeout)
-        # Response headers
+
+@web.route('/scan')
+def scan():
+    tasks.enumerate_subdomains.delay()
+    return 'Scanning domains'
+
+
+@web.route('/scan/headers')
+def scan_headers():
+    tasks.header_scan_subdomains.delay()
+    return 'Initiated Celery background task to scan all domains.'
