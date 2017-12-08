fe4b866846c4fd90ffad4bc3d776cb09b3cf11f8
Daniel Hartnell <danielhartnell@gmail.com>
2017-12-07 19:27:36 -0800

    removed unused methods in database module

--- autobounty/database/__init__.py
+++ autobounty/database/__init__.py
@@ -5,17 +5,6 @@ from autobounty.website.dashboard import web
 
 MONGO = PyMongo(web)
 
-# def find_all_companies():
-#     connection = MONGO.db.domains
-#     companies = []
-#     for company in connection.find():
-#         companies.append({
-#             'company_id': company['company_id'],
-#             'company_name': company['company_name'],
-#             'fqdn': company['fqdn'],
-#             'last_scan': company['last_scan']
-#         })
-#     return companies
 
 def find_domains_by_id(id):
     connection = MONGO.db.domains
@@ -31,50 +20,3 @@ def find_domains_by_id(id):
             'scan_results': e['scan_results']
         })
     return domain[0]
-
-def update_scan(fqdn, scan_results):
-    connection = MONGO.db.domains
-    connection.update_one({
-        'fqdn': fqdn
-    },{
-        '$set': {
-            'scan_results': scan_results
-        }
-    }, upsert=True)
-
-def find_domains_by_company_id(id):
-    connection = MONGO.db.domains
-    domains = []
-    for domain in connection.find({'company_id': id}):
-        domains.append({
-            'fqdn': domain['fqdn'],
-            '_id': domain['_id']
-        })
-    return domains
-
-def find(query):
-    connection = MONGO.db.domains
-    domain = connection.find(query)
-    return domain
-
-def insert(data):
-    query = {'fqdn': data['fqdn']}
-    MONGO.db.domains.update(query, data, upsert=True)
-    return True
-
-def update(data):
-    query = {'fqdn': data['fqdn']}
-    MONGO.db.domains.update(query, data)
-    return True
-
-def delete(fqdn):
-    MONGO.db.domains.delete_one( {'fqdn': fqdn} )
-    return True
-
-def count(company_name):
-    domains = []
-    query = {'company_name': company_name}
-    search = MONGO.db.domains.find(query)
-    for result in search:
-        domains.append(result)
-    return len(domains)
